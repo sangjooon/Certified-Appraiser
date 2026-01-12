e1e47a7d7d0f15e94371e1d65fc6f18af69da3bc
PARK SANG JUN <sangjunpark.dev@gmail.com>
2026-01-12 22:24:29 +0900

    Update msbsd.py

--- msbsd.py
+++ msbsd.py
@@ -2,3 +2,22 @@
 
 import streamlit as st
 import pandas as pd
+
+
+
+
+
+
+
+#main 함수
+def main():
+    st.set_page_config(
+        page_title="문서 비서",
+        page_icon="📄"
+    )
+
+
+
+#실행
+if __name__ == "__main__":
+    main
